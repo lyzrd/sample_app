1dbca28cac01c123c40c11076698a468d9dfc5c4
Jochen <jochen.goertler@gmx.de>
2012-02-19 19:11:02 +0100

    Done with user edit/update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,8 @@ module SessionsHelper
   def sign_in(user)
     #cookies.permanent.signed[:remember_token] = [user.id, user.salt]
     #current_user = user
-    session[:current_user_id] = user.id
+    session[:user_id] = user.id
+    self.current_user = user
   end
   
   # Finds the User with the ID stored in the session with the key
@@ -11,8 +12,8 @@ module SessionsHelper
   # a Rails application; logging in sets the session value and
   # logging out removes it.
   def current_user
-    @_current_user ||= session[:current_user_id] &&
-    User.find_by_id(session[:current_user_id])
+    @current_user ||= session[:user_id] &&
+    User.find_by_id(session[:user_id])
   end
   
   # setter for current user
@@ -25,16 +26,38 @@ module SessionsHelper
   end
   
   def sign_out
-    cookies.delete(:remember_token)
-    current_user = nil
+    session[:user_id] = nil
+    self.current_user = nil
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
   
   private
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
+  
+    def store_location
+      session[:return_to] = request.fullpath
     end
     
-    def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
+    def clear_return_to
+      session[:return_to] = nil
     end
+#    def user_from_remember_token
+#      User.authenticate_with_salt(*remember_token)
+#    end
+#    
+#    def remember_token
+#      cookies.signed[:remember_token] || [nil, nil]
+#    end
 end
